cb946bb84398d978b58a0056c9e9d9fa763d4d82
Simon Ashbery <si.ashbery@gmail.com>
2017-06-01 16:57:47 +0100

    Added abilitiy to filter by tags

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,5 +1,3 @@
-require 'data_mapper'
-require 'dm-postgres-adapter'
 
 class Link
   include DataMapper::Resource
@@ -11,7 +9,3 @@ class Link
   property :url,   String
 
 end
-
-DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
-DataMapper.finalize
-DataMapper.auto_upgrade!
